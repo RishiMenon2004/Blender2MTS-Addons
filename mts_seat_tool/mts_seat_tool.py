333da8581fd036ed51dd557f4d1b9c0d584865c5
RishiMenon2004 <rishimenon82004@gmail.com>
2023-03-26 14:27:50 +0530

    i stoopid, why didn't i copy paste the changes here smh

--- mts_seat_tool/mts_seat_tool.py
+++ mts_seat_tool/mts_seat_tool.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "MTS/IV Seat Tools",
     "author": "Turbo Defender",
-    "version": (1, 0),
+    "version": (2, 0),
     "blender": (2, 90, 0),
     "location": "Object Properties -> MTS/IV Seat Properties",
     "description": "Exports Blender markers as MTS/IV part positions for seats",
@@ -27,7 +27,7 @@ bl_info = {
 }
 
 import bpy
-from bpy.types import Panel
+from bpy.types import (Panel, UIList)
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import (
     BoolProperty,
@@ -35,7 +35,10 @@ from bpy.props import (
     FloatProperty,
     FloatVectorProperty,
     IntProperty,
+    EnumProperty,
+    CollectionProperty,
 )
+import json
 
 #Operator: Exporter
 class MTS_OT_ExportSeat(bpy.types.Operator, ExportHelper):
@@ -47,24 +50,25 @@ class MTS_OT_ExportSeat(bpy.types.Operator, ExportHelper):
     filename_ext = ".json"
 
     def execute(self, context):
-        firstEntry = True
+        # firstEntry = True
         self.report({'INFO'}, "Export Started")
         f = open(self.filepath, "w")
         
+        self.parts = []
+        
         #Write parts section
-        f.write("\"parts\": [\n")
-        firstEntry = True
         for obj in context.scene.objects:
             if obj.mts_seat_settings.isSeat != False:
                 if obj.mts_seat_settings.dismountOffset == None:
                     self.report({'INFO'}, "dismountPos was not defined for %s" % (obj.name))
-                if firstEntry:
-                    firstEntry = False
-                    f.write("    {\n")
-                else:
-                    f.write(",\n    {\n")
+
                 self.export_seat(obj, obj.mts_seat_settings, f, context)
-        f.write("\n],")
+        
+        json.dump({
+            'parts': self.parts
+        }, f, indent=4)
+        
+        f.close()
         
         self.report({'OPERATOR'}, "Export Complete")
 
@@ -75,7 +79,11 @@ class MTS_OT_ExportSeat(bpy.types.Operator, ExportHelper):
         dismountOffset = colset.dismountOffset.copy()
         isWorldSpace = colset.isWorldSpace
         isController = colset.isController
-        
+        isPermanent = colset.isPermanent
+        forceCameras = colset.forceCameras
+        playerScale = colset.playerScale
+        seatEffects = colset.seatEffects
+
         if(not isWorldSpace[0]):
             dismountOffset[0] += obj.location[0]
         if(not isWorldSpace[1]):
@@ -83,18 +91,73 @@ class MTS_OT_ExportSeat(bpy.types.Operator, ExportHelper):
         if(not isWorldSpace[2]):
             dismountOffset[2] += obj.location[2]
         
-        f.write("        \"pos\": [%s, %s, %s],\n" % (round(obj.location[0],5), round(obj.location[2],5), -1*round(obj.location[1],5)))
-
-        f.write("        \"types\": [\"seat\"],\n")
-        
-        f.write("        \"dismountPos\":[%s, %s, %s]" % (round(dismountOffset[0],5), round(dismountOffset[2],5), -1*round(dismountOffset[1],5)))
+        seat = {
+            'pos': [round(obj.location[0],5), round(obj.location[2],5), -1*round(obj.location[1],5)],
+            'types': ["seat"],
+            'dismountPos': [round(dismountOffset[0],5), round(dismountOffset[2],5), -1*round(dismountOffset[1],5)],
+            'linkedParts': []
+        }
         
         if isController:
-            f.write(",\n        \"isController\": true")
-        
-        f.write(",\n        \"linkedDoors\": []")
+            seat['isController'] = True
             
-        f.write("\n    }")
+        if isPermanent:
+            seat['isPermanent'] = True
+            
+        if forceCameras:
+            seat['forceCameras'] = True
+            
+        if playerScale != 1:
+            seat['playerScale'] = playerScale
+            
+        if len(seatEffects) > 0:
+            effects = []
+            for effect in seatEffects:
+                print(effect.name)
+                seatEffect = {
+                    'name': effect.name,
+                    'duration': effect.duration,
+                    'amplifier': effect.amplifier
+                }
+                
+                effects.append(seatEffect)
+            
+            seat['seatEffects'] = effects.copy()
+            
+        self.parts.append(seat)
+#Operator: Add Seat Effect
+class MTS_OT_AddSeatEffect(bpy.types.Operator):
+    bl_idname = "mts.add_seat_effect"
+    bl_label = "(MTS/IV) Add Seat Effect"
+    bl_description = "Add a new potion effect to the selected seat object"
+    
+    def execute(self, context):
+        obj = context.object
+        if obj.mts_seat_settings.isSeat:
+            obj.mts_seat_settings.seatEffects.add()
+            obj.mts_seat_settings.seatEffects[-1].name = "speed"
+            
+        return {'FINISHED'}
+
+class MTS_OT_DeleteSeatEffect(bpy.types.Operator):
+    bl_idname = "mts.remove_seat_effect"
+    bl_label = "(MTS/IV) Remove Seat Effect"
+    bl_description = "Delete the active seat effect from the seat"
+    
+    @classmethod
+    def poll(cls, context):
+        return context.object.mts_seat_settings.seatEffects
+    
+    def execute(self, context):
+        obj = context.object
+        seat_effects = obj.mts_seat_settings.seatEffects
+        index = obj.mts_seat_settings.seatEffectsIndex
+        
+        seat_effects.remove(index)
+        new_index = min(max(0, index - 1), len(seat_effects) - 1)
+        obj.mts_seat_settings.seatEffectsIndex = max(new_index, 0)
+        
+        return {'FINISHED'}
 
 #Operator: Create an one dimensional array of selected seat objects in the y axis
 class MTS_OT_SeatArray(bpy.types.Operator):
@@ -207,21 +270,215 @@ class MTS_OT_DismountPreview(bpy.types.Operator):
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
+class PotionEffects(bpy.types.PropertyGroup):
+    name: EnumProperty (
+        name = "Name",
+        default = "speed",
+        items = [
+            (
+                "speed", 
+                "Speed",
+                ""
+            ),
+            (
+                "slowness", 
+                "Slowness",
+                ""
+            ),
+            (
+                "haste", 
+                "Haste",
+                ""
+            ),
+            (
+                "mining_fatigue", 
+                "Mining Fatigue",
+                ""
+            ),
+            (
+                "strength", 
+                "Strength",
+                ""
+            ),
+            (
+                "instant_health", 
+                "Instant Health",
+                ""
+            ),
+            (
+                "instant_damage", 
+                "Instant Damage",
+                ""
+            ),
+            (
+                "jump_boost", 
+                "Jump Boost",
+                ""
+            ),
+            (
+                "nausea", 
+                "Nausea",
+                ""
+            ),
+            (
+                "regeneration", 
+                "Regeneration",
+                ""
+            ),
+            (
+                "resistance", 
+                "Resistance",
+                ""
+            ),
+            (
+                "fire_resistance", 
+                "Fireresistance",
+                ""
+            ),
+            (
+                "water_breathing", 
+                "Water_Breathing",
+                ""
+            ),
+            (
+                "invisibility", 
+                "Invisibility",
+                ""
+            ),
+            (
+                "blindness", 
+                "Blindness",
+                ""
+            ),
+            (
+                "night_vision", 
+                "Night Vision",
+                ""
+            ),
+            (
+                "hunger", 
+                "Hunger",
+                ""
+            ),
+            (
+                "weakness", 
+                "Weakness",
+                ""
+            ),
+            (
+                "poison", 
+                "Poison",
+                ""
+            ),
+            (
+                "wither", 
+                "Wither",
+                ""
+            ),
+            (
+                "health_boost", 
+                "Health Boost",
+                ""
+            ),
+            (
+                "absorption", 
+                "Absorption",
+                ""
+            ),
+            (
+                "saturation", 
+                "Saturation",
+                ""
+            ),
+            (
+                "glowing", 
+                "Glowing",
+                ""
+            ),
+            (
+                "levitation", 
+                "Levitation",
+                ""
+            ),
+            (
+                "luck", 
+                "Luck",
+                ""
+            ),
+            (
+                "unluck", 
+                "Unluck",
+                ""
+            ),
+        ]
+    )
+    
+    duration: IntProperty(
+        name = "Duration",
+        default = 5,
+        min = 1
+    )
+    
+    amplifier: IntProperty(
+        name = "Amplifier",
+        default = 0,
+        min = 0,
+        max = 255
+    )
+
 #Create the custom properties for seat markers
 class SeatSettings(bpy.types.PropertyGroup):
     
     isSeat: BoolProperty(
-        name = "Is Door",
+        name = "Is Seat",
         default = False
-        )
+    )
     
+    isController: BoolProperty(
+        name = "Is Controller",
+        default = False
+    )
+    
+    isPermanent: BoolProperty(
+        name = "Is Permanent",
+        default = False
+    )
+    
+    canDisableGun: BoolProperty(
+        name = "Can Disable Gun",
+        default = False
+    )
+    
+    forceCameras: BoolProperty(
+        name = "Force Cameras",
+        default = False
+    )
+    
+    playerScale: FloatProperty(
+        name = "Player Scale",
+        default = 1,
+        min = 0.125,
+        max = 999,
+        soft_max = 100
+    )
+    
+    seatEffects: CollectionProperty(
+        name = "Seat Effects",
+        type = PotionEffects
+    )
+
+    seatEffectsIndex: IntProperty(
+        name = "Seat Effect Index",
+        
+    )
+
     dismountOffset: FloatVectorProperty(
-        name = "Dismount Pos",
+        name = "Dismount Offset",
         default = [0.0, 0.0, 0.0],
         precision = 5,
         step = 100,
         subtype = 'XYZ'
-        )
+    )
         
     isWorldSpace: BoolVectorProperty(
         name = "Is World Space",
@@ -229,10 +486,21 @@ class SeatSettings(bpy.types.PropertyGroup):
         subtype = 'XYZ'
     )
 
-    isController: BoolProperty(
-        name = "Is Controller",
-        default = False
-        )
+class MTS_UL_SeatEffectsList(UIList):
+    bl_idname = "MTS_UL_seatEffectsList"
+    
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        custom_icon = 'EXPERIMENTAL'
+        
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            if index == context.object.mts_seat_settings.seatEffectsIndex:
+                layout.prop(item, "name", text="", emboss=False, icon=custom_icon)
+            else:
+                layout.label(text=item.name, icon=custom_icon)
+            
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon=custom_icon)
 
 #Panel: Draw the parent panel for the collision boxes and door hitboxes
 class MTS_PT_MTSSeatPanel(Panel):
@@ -252,24 +520,71 @@ class MTS_PT_MTSSeatPanel(Panel):
         #get it's custom properties
         seatsettings = obj.mts_seat_settings
         
-        row = layout.row()
+        box = layout.row()
         #export operator button
-        row.operator(icon='EXPORT', operator="mts.export_seat_pos")
+        box.operator(icon='EXPORT', operator="mts.export_seat_pos")
         
         row = layout.row()
+        row.label(text="Seat Properties")
+        
+        box = layout.box()
+        
+        row = box.row()
+        row.prop(seatsettings, "isSeat", icon="MOD_SKIN")
+        
         #check if the seat property is enabled if so show the name property
-        row.prop(seatsettings, "isSeat", text="Is Seat")
         if seatsettings.isSeat == True:
             #add the custom properties
             #controller seat
-            row.prop(seatsettings, "isController", text="Is Controller")    
+            row.prop(seatsettings, "isController", icon="MODIFIER")    
+            
+            row = box.row()
+            row.prop(seatsettings, "isPermanent", icon="MODIFIER")    
+            row.prop(seatsettings, "canDisableGun", icon="MODIFIER")    
+            
+            row = box.row()
+            row.prop(seatsettings, "playerScale", icon="MODIFIER")    
+            
             row = layout.row()
-            #dismount position
-            row.prop(seatsettings, "dismountOffset", text="Dismount Offset")
+            row.label(text="Seat Effects")
+            
+            listRow = layout.row()
+            
+            column = listRow.column()
+            
+            box = column.box()
+            
+            box.template_list("MTS_UL_seatEffectsList", "Seat Potion Effects", seatsettings, "seatEffects", seatsettings, "seatEffectsIndex")
+            if seatsettings.seatEffects and len(seatsettings.seatEffects) >= 0:
+                activeItem = seatsettings.seatEffects[seatsettings.seatEffectsIndex]
+                
+                row = box.row()
+                row.prop(activeItem, "name", text="Effect Name")
+                
+                row = box.row()
+                row.prop(activeItem, "duration")
+                row.prop(activeItem, "amplifier")
+                
+            column = listRow.column()
+            column.operator("mts.add_seat_effect", icon='ADD', text="")
+            column.operator("mts.remove_seat_effect", icon='REMOVE', text="")
+            
+            layout.separator()
+            
             row = layout.row()
-            #use world space
-            row.prop(seatsettings, "isWorldSpace", text="Use World Space")
-
+            row.label(text="Dismount Position")
+            
+            box = layout.box()
+            row = box.row()
+            row.column().prop(seatsettings, "dismountOffset", text="X {}".format("Pos" if seatsettings.isWorldSpace[0] else "Offset"), index=0)
+            row.column().prop(seatsettings, "isWorldSpace", text="Use World Space", index=0, toggle=True, icon="ORIENTATION_GLOBAL")
+            row = box.row()
+            row.column().prop(seatsettings, "dismountOffset", text="Y {}".format("Pos" if seatsettings.isWorldSpace[1] else "Offset"), index=1)
+            row.column().prop(seatsettings, "isWorldSpace", text="Use World Space", index=1, toggle=True, icon="ORIENTATION_GLOBAL")
+            row = box.row()
+            row.column().prop(seatsettings, "dismountOffset", text="Z {}".format("Pos" if seatsettings.isWorldSpace[2] else "Offset"), index=2)
+            row.column().prop(seatsettings, "isWorldSpace", text="Use World Space", index=2, toggle=True, icon="ORIENTATION_GLOBAL")
+            
 #Panel: Parent for drawing the main MTS/IV tab in the numbers panel
 class View3DPanel:
     bl_space_type = 'VIEW_3D'
@@ -302,9 +617,13 @@ def menu_func_export(self, context):
 
 classes = (
     MTS_OT_ExportSeat,
+    MTS_OT_AddSeatEffect,
+    MTS_OT_DeleteSeatEffect,
     MTS_OT_SeatArray,
     MTS_OT_DismountPreview,
+    PotionEffects,
     SeatSettings,
+    MTS_UL_SeatEffectsList,
     MTS_PT_MTSSeatPanel,
     MTS_V3D_SeatPanel,
 )
